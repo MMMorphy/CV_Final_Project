d91f283eb935f63135252c82386cee2c650f5a43
“Lagrangedada” <yida20050621@gmail.com>
2025-01-10 11:49:41 +0800

    工具函数

---
+++ utils.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def gram_matrix(x):
+    (b, ch, h, w) = x.size()
+    features = x.view(b, ch, w * h)
+    features_t = features.transpose(1, 2)
+    gram = features.bmm(features_t) / (ch * h * w)
+    return gram
